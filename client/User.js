2d00b5146f8676dea2a78962d4869fe8c2e37113
fiatjaf <fiatjaf@gmail.com>
2018-12-24 21:18:36 +0000

    fix user page listings.

--- client/User.js
+++ client/User.js
@@ -19,13 +19,11 @@ export default function Home(props) {
     <>
       <section>
         <h1>{owner}</h1>
-        <table>
-          <tbody>
-            {entries.map(entry => (
-              <RecordRow key={entry.owner + '/' + entry.name} {...entry} />
-            ))}
-          </tbody>
-        </table>
+        <div id="records">
+          {entries.map(entry => (
+            <RecordRow key={entry.owner + '/' + entry.name} {...entry} />
+          ))}
+        </div>
       </section>
     </>
   )
